f36be9132abbb0d4fcfda7e6a09caff5c1f00fb5
Garion Cox <garion.cox@students.snow.edu>
2024-11-07 08:37:36 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,10 +11,5 @@ export default defineConfig({
         changeOrigin: true,
       },
     }
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
   },
 })
