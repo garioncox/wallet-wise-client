7d50f5e89c99010d130c21290a8ea0d33fe23917
Garion Cox <garion.cox@students.snow.edu>
2024-11-06 21:18:16 -0700

    Added api request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import { Customer } from "./Data/Customer";
+import axios from "axios";
+
 function App() {
+  const [user, setUser] = useState<Customer | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      console.log("fetching user...");
+      const response = await axios.get(`/api/Customer/getAll`);
+      setUser(response.data[0]);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <>
       <p>Hello World!</p>
+      {user && <p>{user.surname}</p>}
     </>
   );
 }
