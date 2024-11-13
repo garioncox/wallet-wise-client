2beefd91fb8d68e46886ad0b3e525657ae688228
Garion Cox <garion.cox@students.snow.edu>
2024-11-13 09:43:46 -0700

    Replace args on signin callback

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { AuthProvider } from "react-oidc-context";
+import { AuthProvider, AuthProviderProps } from "react-oidc-context";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./Functions/TanStack/QueryClient.ts";
 
-const oidcConfig = {
+const oidcConfig: AuthProviderProps = {
   authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
   client_id: "garion-final",
   redirect_uri: "https://garion-final.duckdns.org/",
+  onSigninCallback: (user) => {
+    console.log(user);
+    window.history.replaceState({}, document.title, window.location.pathname);
+  },
   automaticSilentRenew: true,
 };
 
