4992f5a528d8a7fc26d534284c46f483ae3c6ed8
Garion Cox <garion.cox@students.snow.edu>
2024-11-20 15:03:11 -0700

    Auth checkpoint

--- src/main.tsx
+++ src/main.tsx
@@ -12,8 +12,11 @@ import { BrowserRouter } from "react-router-dom";
 
 const oidcConfig: AuthProviderProps = {
   authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
-  client_id: "garion-final",
-  redirect_uri: "https://garion-final.duckdns.org/",
+  client_id: "garion-auth-class",
+  redirect_uri:
+    process.env.NODE_ENV === "production"
+      ? "https://garion-final.duckdns.org/"
+      : "http://localhost:5173",
   onSigninCallback: (user) => {
     console.log(user);
     window.history.replaceState({}, document.title, window.location.pathname);
