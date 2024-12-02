ea2ed603f68039ba3f4c8c135c269719215c885f
Garion Cox <garion.cox@students.snow.edu>
2024-12-02 14:53:37 -0700

    Added more content to profile page

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,17 +1,30 @@
-import { useAuth } from "react-oidc-context";
 import { Cardify } from "../Components/Layout/Cardify";
 import { Spinner } from "../Components/Layout/Spinner";
+import { useAllBudgetForCurrentCustomer } from "../Functions/TanStack/BudgetQueries";
+import { useCurrentCustomer } from "../Functions/TanStack/CustomerQueries";
+import { useAllTransactionEventsForCurrentCustomer } from "../Functions/TanStack/TransactionQueries";
 
 export const Profile = () => {
-  const { user, isLoading } = useAuth();
+  const { data: user, isLoading } = useCurrentCustomer();
+  const { data: budgets, isLoading: isBudgetsLoading } =
+    useAllBudgetForCurrentCustomer();
+  const { data: transactions, isLoading: isTransactionsLoading } =
+    useAllTransactionEventsForCurrentCustomer();
 
-  if (isLoading) {
+  if (isLoading || isBudgetsLoading || isTransactionsLoading) {
     return <Spinner />;
   }
 
   return (
     <Cardify>
-      <div>{user?.profile.email}</div>
+      <div>
+        <div className="text-center  border-b-2 border-stone-500 pb-3 mb-3">
+          <div className="font-semibold text-xl">{user.surname}</div>
+          <div>{user.email}</div>
+        </div>
+        <div>{budgets.length} budgets</div>
+        <div>{transactions.length} transactions</div>
+      </div>
     </Cardify>
   );
 };
