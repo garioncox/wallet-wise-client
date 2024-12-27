bd9b226cc5aa27968afb040c428c76d76d228816
Garion Cox <garion.cox@students.snow.edu>
2024-12-27 10:32:13 -0700

    Fade in on first render

--- src/Components/Layout/MainLayout.tsx
+++ src/Components/Layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "./Navbar";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
@@ -8,10 +8,20 @@ export const MainLayout = (props: { children: React.ReactNode }) => {
     ? "http://localhost:5046"
     : import.meta.env.VITE_DB_ADDR;
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="flex grow w-full bg-stone-100 justify-center items-center">
+      <div
+        className={`flex grow w-full bg-stone-100 justify-center items-center transition-opacity duration-500 ease-in-out ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        }`}
+      >
         {props.children}
       </div>
       <Toaster position="bottom-right" reverseOrder={false} />
